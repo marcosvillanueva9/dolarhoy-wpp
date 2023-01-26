ec14402ca123f51ca7275553dd9dd6c390b36881
Initial Install <marcosvillanueva009@gmail.com>
2023-01-26 16:28:08 -0300

    add command to make and alias in bash

--- main.go
+++ main.go
@@ -2,56 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"strings"
 
-	"github.com/gin-gonic/gin"
 	"github.com/marcosvillanueva9/dolarhoy-wpp/scrapper"
-	"github.com/marcosvillanueva9/dolarhoy-wpp/send"
-	"github.com/robfig/cron/v3"
 )
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-
-		// Get Dolar Value
 		valordolar := scrapper.Run()
 
-		c.JSON(http.StatusOK, gin.H{"data": formatMessage(valordolar)})
-	})
-
-	r.GET("/send", func(c *gin.Context) {
-
-		// Get Dolar Value
-		valordolar := scrapper.Run()
-
-		mensaje := formatMessage(valordolar)
-
-		send.Run(mensaje)
-
-		c.JSON(http.StatusOK, gin.H{"data": "sent: " + mensaje})
-	})
-
-	go func() {
-		c := cron.New()
-		c.AddFunc("@every 1d", func() { 
-
-			// Get Dolar Value
-			valordolar := scrapper.Run()
-
-			mensaje := formatMessage(valordolar)
-
-			send.Run(mensaje)
-		})
-
-		// Start cron with one scheduled job
-		fmt.Println("Start cron")
-		c.Start()
-	}()
-
-	r.Run()
+		fmt.Println(formatMessage(valordolar))
 }
 
 func formatMessage(valordolar string) string{
